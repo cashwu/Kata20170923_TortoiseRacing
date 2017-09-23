2c9cdd33674d87c24f6b5d41a893496c29b28f6c
cash <cash@cashwu.com>
2017-09-23 15:09:21 +0800

    init

--- Kata20170923_TortoiseRacing/TortoiseRacingTests.cs
+++ Kata20170923_TortoiseRacing/TortoiseRacingTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170923_TortoiseRacing
 {
     [TestClass]
-    public class UnitTest1
+    public class TortoiseRacingTests
     {
         [TestMethod]
         public void TestMethod1()
