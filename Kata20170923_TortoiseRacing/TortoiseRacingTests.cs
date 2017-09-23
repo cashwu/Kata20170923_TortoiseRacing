e391219d2ef467c613cc04e9b808154a61b511b2
cash <cash@cashwu.com>
2017-09-23 15:24:33 +0800

    input v1 1, v2 2 and lead 2 test case

--- Kata20170923_TortoiseRacing/TortoiseRacingTests.cs
+++ Kata20170923_TortoiseRacing/TortoiseRacingTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170923_TortoiseRacing
@@ -7,8 +8,24 @@ namespace Kata20170923_TortoiseRacing
     public class TortoiseRacingTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_v1_1_v2_2_and_lead_2_should_return_2_0_0()
         {
+            TortoiseRaceShouldBe(new[] { 2, 0, 0 }, 1, 2, 2);
+        }
+
+        private static void TortoiseRaceShouldBe(int[] expected, int v1, int v2, int g)
+        {
+            var tortoise = new Tortoise();
+            var actual = tortoise.Race(v1, v2, g);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Tortoise
+    {
+        public int[] Race(int v1, int v2, int g)
+        {
+            return new[] { 2, 0, 0 };
         }
     }
 }
